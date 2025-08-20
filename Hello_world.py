06eb3a4bc4318a775717c235fd43c420ee381be6
Zuzanune <zane.carter@ucas-edu.net>
2025-08-20 09:51:22 -0600

    rereremade hello_world.py

--- Hello_world.py
+++ Hello_world.py
@@ -2,9 +2,9 @@
 name1 = input("what is your name: ")
 if name1 == "zane" or "Zane":
     print ("welcome ", name1, "booting up admin servers")
-if name1 == "Mrs Larose":
+elif name1 == "Mrs Larose":
     print ("welcome ", name1, "booting up admin servers")
-if name1 == "Alex" or "Larry" or "Kingsley" or "Lucas" or "Levi":
+elif name1 == "Alex" or "Larry" or "Kingsley" or "Lucas" or "Levi":
     print ("welcome", name1, " admin password required") 
 else:
     print ("welcome to Wallys warehouse's website ", name1)
