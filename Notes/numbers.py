a5606c32c0b560eb0625e45ddd6eb37730c3b247
Zuzanune <zane.carter@ucas-edu.net>
2025-08-27 09:54:13 -0600

    did my number notes

--- Notes/numbers.py
+++ Notes/numbers.py
@@ -1,2 +1,13 @@
 # ZC 1st integers and floats Notes
 print((4+3.1*10) + 5 * 3 * -20 %2)
+#Integers:
+int1 = 4
+int2 = -4
+int3 = 2000
+#floats:
+flt1 = 3.5
+flt2 = 4.0
+flt3 = 100.741
+
+
+#use variables in 
