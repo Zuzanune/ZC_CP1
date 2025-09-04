9d81e00a84fb6915199d658a2ccfdc35c079d714
Zuzanune <zane.carter@ucas-edu.net>
2025-09-04 09:52:39 -0600

    took notes on string methods

---
+++ Notes/String_methods.py
@@ -0,0 +1,25 @@
+# ZC 1st String Method Notes
+
+#string = input("write something:  ")
+#a string method is something that changes the string
+#print ((string.upper()) + "!")
+#but it is not really changed
+#print (string)
+#but users are idiots, so you have to idiot proof your code
+#string2 = input("write another thing")
+#print (string2)
+#string2 = string2.strip()
+#print (string2)
+#string2 = string2.lower()
+#print (string2)
+#F_STRINGS!!!!!!!
+#print (f"I do not know what {string2} or {string} mean!")
+#indexing!
+Words = "how much wood would a wood chuck chuck if a wood chuck could chuck wood?"
+word = input("tell me a word")
+num = (Words.index(word))
+
+
+
+
+
