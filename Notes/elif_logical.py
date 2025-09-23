ecc79871900a5e167c95bb4a7c65bf616a179311
Zuzanune <zane.carter@ucas-edu.net>
2025-09-23 09:50:19 -0600

    did my Notes on elif and llogical operators

---
+++ Notes/elif_logical.py
@@ -0,0 +1,16 @@
+#ZC 1st elif and logical operators notes
+grade = float(input("what is your grade:  "))
+if grade > 100:
+    print ("Wow! you got extra-credit! amazing")
+elif grade == 100.0:
+    print ("A perfect Score! great job!")
+elif float(grade) >91.5 and grade < 100:
+    print ("you got an A! great job!")
+elif float(grade) >= 89 and grade < 91.5:
+    print ("a A-. not bad")
+elif float(grade) >= 83 and grade < 89:
+    print ("a B+ or a B is not bad...")
+elif grade < 0.0:
+    print ("what? how! you got less then zero?")
+else:
+    print ("you need to work on your grades")
