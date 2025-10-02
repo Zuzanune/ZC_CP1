a77fe3a4ff766485a62bafe6f784fe752bcf4b21
Zuzanune <zane.carter@ucas-edu.net>
2025-10-02 09:45:57 -0600

    completed the multiplication table

--- Practise/multiplication_table.py
+++ Practise/multiplication_table.py
@@ -1,5 +1,21 @@
 #ZC 1st multiplication table project
+cnum = 0
+running = True
 nums = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+nums2 =[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+totalist = []
+tlist = []
 for x in nums:
+    cnum = x
+    for y in nums2:
+        total = x * y
+        totalist.append(total)
+
+    tlist.extend(totalist)
+    tlist.append("\n")
+    totalist = []
+for x in tlist:
+    print(str(x),end=" ")
+    
     
 
