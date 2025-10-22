d3276da23be25a21a0d69b396e0d942b7a2f92de
Zuzanune <Zane.carter@UCAS-edu.net>
2025-10-22 16:50:53 -0600

    Update Caesar_Cypher.py

--- Practise/Caesar_Cypher.py
+++ Practise/Caesar_Cypher.py
@@ -1,26 +1,32 @@
-#ZC 1st Caesar Cipher
-#create start lists of alphebet
-alphebet = ["abcdefghijklmnopqrstuvwxyz"]
+# ZC 1st Caesar Cipher
+
+# create start list of alphabet
+alphebet = "abcdefghijklmnopqrstuvwxyz"
 enstring = ""
-#ask user what they are encoding
+
+# ask user what they are encoding
 string = input("what are you trying to encode/decode:  ")
-#create function for encode
+
+# create function for encode
 def code(Encodenum, enstring):
-    newlet=0
-    #ask what they want to decode/encode
-    #find value in alphebet and increase it by selected amount, making sure it loops oiver if past 26
+    newlet = ""
+    # find value in alphabet and increase it by selected amount, looping if past 26
     for x in string:
-        x.lower()
+        x = x.lower()
         if x in alphebet:
             letter_val = alphebet.index(x)
             encodednum = letter_val + Encodenum
-            if encodednum > 26:
+            if encodednum >= 26:
                 encodednum -= 26
             newlet = alphebet[encodednum]
-    return enstring + str(newlet)
-#call encode function
+            enstring += newlet
+        else:
+            enstring += x  # keep non-letter characters
+    return enstring
+
+# call encode function
 alincrease = int(input("by which number do you want to encode your message along the Caesar cipher:  "))
-enstring = code(alincrease,enstring)
-#print encoded string
-print (f"the encoded message is now {enstring}")
-        
+enstring = code(alincrease, enstring)
+
+# print encoded string
+print(f"the encoded message is now {enstring}")
