3115e815409fdbcc18815d78c8a07619924e92c9
Zuzanune <zane.carter@ucas-edu.net>
2025-11-14 09:21:09 -0700

    added idiot proofing and defines a functuon

--- Practise/menu.py
+++ Practise/menu.py
@@ -1,43 +1,44 @@
 #ZC 1st menu program
-while True:
-    #list all menu prices using a dictionary
-    menu = {"mains:" : "", "pizza" : 7.99, "beefburger": 10.15, "spiceburger" : 11.50,"chezburg" : 5.99, "nacho" : 4.50, "nacho delux" : 6.50, "sides:" : "",  "fries" : 4.00, "fountain drink" : 2.99, "water" : 0, "iced cream" : 4.50, "condements:" : "",  "ketchup" : 0.25, "mustard" : 0.25}
-    order = []
-    cost = 0
-    #display menu and price
-    for dis in menu:
-        print (f"{dis} ${menu[dis]}")
-    #while loop for item selections.
+def wamburger():
     while True:
-        #ask user for item
-        added = input("what do you want to add to your order:  ")
-        if added not in menu:
-            print(f" i am sorry, we do not serve {added}")
-            continue
-        #add price to total
-        cost += menu[added]
-        order.append(added)
-        #ask user if they are done adding items
-        end = input("is this all(y/n):  ")
-        if end.lower() == "y" or end.lower() == "yes":
-            #if they are, quit
-            break
-        else:
-            continue
-    #print total menu and cost
-    print ("this is your order:")
-    for ic in order:
-        print (f"you orderd:  {ic}")
-    print (f"total cost: {cost}")
+        #list all menu prices using a dictionary
+        menu = {"mains:" : "", "pizza" : 7.99, "beefburger": 10.15, "spiceburger" : 11.50,"chezburg" : 5.99, "nacho" : 4.50, "nacho delux" : 6.50, "sides:" : "",  "fries" : 4.00, "fountain drink" : 2.99, "water" : 0, "iced cream" : 4.50, "condements:" : "",  "ketchup" : 0.25, "mustard" : 0.25}
+        order = []
+        cost = 0
+        #display menu and price
+        for dis in menu:
+         print (f"{dis} ${menu[dis]}")
+          #while loop for item selections.
+        while True:
+            #ask user for item
+            added = input("what do you want to add to your order:  ").lower().strip()
+            if added not in menu:
+                print(f" i am sorry, we do not serve {added}")
+                continue
+            #add price to total
+            cost += menu[added]
+            order.append(added)
+            #ask user if they are done adding items
+            end = input("is this all(y/n):  ").lower().strip()
+            if end == "y" or end == "yes":
+                #if they are, quit
+                break
+            else:
+                continue
+        #print total menu and cost
+        print ("this is your order:")
+        for ic in order:
+            print (f"you orderd:  {ic}")
+        print (f"total cost: {cost}")
     
-    #ask user for a tip of 0% 10% and 30%
-    tip = input("please select a tip of $0, $5, $15, or custom:  ")
-    cost += int(tip)
-    #display new total and ask for comfirmation
-    print(f"your new cost is {cost}")
-    print ("please enter all of your bank credentials, including your password, username, and current balance")
-    input()
-    break
-print ("here is your order. thank you for coming to Wam Burger")
-for en in order:
-    print (f"1 {en}" )
+        #ask user for a tip of 0% 10% and 30%
+        tip = input("please select a tip of $0, $5, $15, or custom:  ").strip()
+        cost += int(tip)
+        #display new total and ask for comfirmation
+        print(f"your new cost is {cost}")
+        print ("please enter all of your bank credentials, including your password, username, and current balance")
+        input()
+        break
+    print ("here is your order. thank you for coming to Wam Burger")
+    for en in order:
+        print (f"1 {en}" )
